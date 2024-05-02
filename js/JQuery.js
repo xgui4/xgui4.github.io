7a6632aaeeb8bac849e83c58a3e02d17f78d93e9
Xgui4 Studio <xgui2004@gmail.com>
2024-05-01 22:22:53 -0400

    ajout footer + navbar

--- js/JQuery.js
+++ js/JQuery.js
@@ -1,3 +1,8 @@
+function add_jqmigrate_script() {
+  wp_register_script('jquery', 'https://code.jquery.com/jquery-3.7.2.min.js', false, true);
+  wp_enqueue_script('jquery');
+}
+
 $(function(){
     $("#header").load("header.html"); 
     $("#footer").load("footer.html"); 
